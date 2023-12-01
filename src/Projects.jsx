ac4ff6eef892ab52bc5073895f6742df9bc6cefa
Darya-s <darya.stepanova95@gmail.com>
2023-12-01 17:38:24 +0100

    changed styles

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -5,14 +5,14 @@ import { Link } from 'react-router-dom';
 
 const Projects = () => {
   return (
-    <div className='absolute top-[92%] left-0 w-full h-24 bg-blue-500 rounded-lg shadow-md z-10'>
-      <div className='  grid text-text justify-center items-center mx-auto '>
+    <div className='absolute top-[92%] left-0 w-full h-24 bg-blue-500 rounded-lg shadow-md z-10 -mt-1'>
+      <div className='grid text-text justify-center items-center mx-auto '>
         <div className='bg-text rounded-tl-lg rounded-tr-lg shadow-top-right'>
           <h2 className='text-4xl font-bold text-main px-8'>My projects</h2>
         </div>
       </div>
 
-      <div className='bg-text shadow-top-right text-main flex justify-center items-center -mt-1 sm:h-screen sm:max-h-[50vh]'>
+      <div className='  bg-text shadow-top-right text-main flex justify-center items-center  sm:h-screen sm:max-h-[50vh]'>
         <div className=' grid grid-cols-1 sm:grid-cols-3 gap-24 mt-20 '>
           <div className=' flex justify-center items-center transition-transform transform hover:scale-125 sm:hover:scale-150 hover:z-10 text-center mx-6'>
             <Link
