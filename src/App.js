1097c42dd796063a0d20043def171a8afc39b079
Darya-s <darya.stepanova95@gmail.com>
2024-09-06 07:48:50 +0200

    added hashrouter for deploy on github pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
-import { Routes, Route, HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Navigation from './Navigation';
-import Home from './Home';
-import Contact from './Contact';
-import NoPage from './NoPage';
-import Skills from './Skills';
-import Certificates from './Certificates';
+import { router } from './Routes';
+
 import Header from './Header';
 import Footer from './Footer';
 
@@ -15,13 +12,7 @@ function App() {
       <HashRouter>
         <Navigation />
         <Header />
-        <Routes>
-          <Route path='/portfolio' element={<Home />} index />
-          <Route path='certificates' element={<Certificates />} />
-          <Route path='skills' element={<Skills />} />
-          <Route path='contact' element={<Contact />} />
-          <Route path='*' element={<NoPage />} />
-        </Routes>
+        {router}
         <Footer />
       </HashRouter>
     </div>
---
+++ src/Routes.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Contact from './Contact';
+import NoPage from './NoPage';
+import Skills from './Skills';
+import Certificates from './Certificates';
+
+export const router = (
+  <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/home' element={<Home />} />
+    <Route path='certificates' element={<Certificates />} />
+    <Route path='skills' element={<Skills />} />
+    <Route path='contact' element={<Contact />} />
+    <Route path='*' element={<NoPage />} />
+  </Routes>
+);
